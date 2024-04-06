13538d51eb5d1116db43cdec3281838783b57835
Bombara - PC <xbombara@stuba.sk>
2024-04-06 11:20:06 +0200

    Nacitanie viacerych obrazkov a ich zobrazenie v liste

--- gui/mainwindow.py
+++ gui/mainwindow.py
@@ -1,7 +1,9 @@
 # This Python file uses the following encoding: utf-8
 import sys
 
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QMainWindow, QListWidgetItem, QFileDialog, QLabel
+from PySide6.QtCore import QSize
+from PySide6.QtGui import QPixmap, QIcon
 
 # Important:
 # You need to run the following command to generate the ui_form.py file
@@ -9,12 +11,34 @@ from PySide6.QtWidgets import QApplication, QMainWindow
 #     pyside2-uic form.ui -o ui_form.py
 from ui_form import Ui_MainWindow
 
-class MainWindow(QMainWindow):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
 
+class MainWindow(QMainWindow):
+     def __init__(self, parent=None):
+         super().__init__(parent)
+         self.ui = Ui_MainWindow()
+         self.ui.setupUi(self)
+
+         self.ui.load_img_button.clicked.connect(self.load_images)
+
+     def load_images(self):
+        file_dialog = QFileDialog(self)
+        file_dialog.setFileMode(QFileDialog.ExistingFiles)
+        file_dialog.setNameFilter("Obrázky (*.png *.jpg *.jpeg *.bmp *.gif)")
+
+        if file_dialog.exec():
+            selected_files = file_dialog.selectedFiles()
+
+            for path in selected_files:
+                image_label = QLabel()
+                pixmap = QPixmap(path)
+
+                desired_size = pixmap.scaledToWidth(200)
+                image_label.setPixmap(desired_size)
+
+                item = QListWidgetItem()
+                item.setSizeHint(QSize(100, 200))
+                self.ui.listWidget.addItem(item)
+                self.ui.listWidget.setItemWidget(item, image_label)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
---
+++ gui/ui_form.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'form.ui'
+##
+## Created by: Qt User Interface Compiler version 6.6.3
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
+    QCursor, QFont, QFontDatabase, QGradient,
+    QIcon, QImage, QKeySequence, QLinearGradient,
+    QPainter, QPalette, QPixmap, QRadialGradient,
+    QTransform)
+from PySide6.QtWidgets import (QApplication, QListWidget, QListWidgetItem, QMainWindow,
+    QMenu, QMenuBar, QPushButton, QSizePolicy,
+    QStatusBar, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.load_img_button = QPushButton(self.centralwidget)
+        self.load_img_button.setObjectName(u"load_img_button")
+        self.load_img_button.setGeometry(QRect(49, 70, 101, 24))
+        self.listWidget = QListWidget(self.centralwidget)
+        self.listWidget.setObjectName(u"listWidget")
+        self.listWidget.setGeometry(QRect(180, 40, 551, 421))
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 800, 21))
+        self.menuMenu = QMenu(self.menubar)
+        self.menuMenu.setObjectName(u"menuMenu")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.menubar.addAction(self.menuMenu.menuAction())
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.load_img_button.setText(QCoreApplication.translate("MainWindow", u"Load...", None))
+        self.menuMenu.setTitle(QCoreApplication.translate("MainWindow", u"Menu", None))
+    # retranslateUi
+
